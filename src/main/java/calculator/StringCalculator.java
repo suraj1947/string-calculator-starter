4a308a09c72e9f37c4f1595c9cf160acc38255ff
Suraj Pohekar <surajpohekar0@gmail.com>
2021-10-17 11:34:51 +0530

    string returnung 1

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -3,7 +3,11 @@ package calculator;
 class StringCalculator {
 
     public int add(String input) {
-        return 0;
+    	if(input.length()!=0) {
+    		int a = Integer.parseInt(input);
+        return a;
+    	}
+    	return 0;
     }
 
 }
