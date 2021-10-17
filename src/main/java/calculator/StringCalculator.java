4c7aa5cd527cba85a77e164bf39b72673e57a659
Suraj Pohekar <surajpohekar0@gmail.com>
2021-10-17 12:32:33 +0530

    addition of two numbers case successfully run'

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,13 +1,21 @@
 package calculator;
 
 class StringCalculator {
-
+	
+	private boolean Empty(String string) {
+		return string.isEmpty();
+	}
+	private String[] stringToInt(String string) {
+		String[] s = string.split(",");
+		return s;
+	}
     public int add(String input) {
-    	if(!input.isEmpty()) {
-    		
+    	if(Empty(input)) 
     		return 0;
-    	}
-    	return Integer.parseInt(input);
+    	else if(input.length()==1)
+    		return Integer.parseInt(input);
+    	String[] s = stringToInt(input);
+    		return Integer.parseInt(s[0])+Integer.parseInt(s[1]);
     }
-
+    
 }
